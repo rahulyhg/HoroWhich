95b79e8a1ff2d040415c229b2345620eb387097f
abuicke <abuicke@treemetrics.com>
2018-04-05 12:42:57 +0100

    think Ive fixed it this time

---
+++ app/src/main/java/ie/moses/horowhich/DebugUtils.java
@@ -0,0 +1,10 @@
+package ie.moses.horowhich;
+
+public final class DebugUtils {
+
+    public static final boolean DEBUG_MODE = true;
+
+    private DebugUtils() {
+    }
+
+}
--- app/src/main/java/ie/moses/horowhich/SharedPreferencesUtils.java
+++ app/src/main/java/ie/moses/horowhich/SharedPreferencesUtils.java
@@ -9,6 +9,8 @@ public final class SharedPreferencesUtils {
 
     private static final String HOROSCOPE_PREFS = "HOROSCOPE_PREFS";
 
+    private static final String TODAYS_HOROSCOPE_SAVE_TIME_KEY = "TODAYS_HOROSCOPE_SAVE_TIME_KEY";
+
     private static final String TODAYS_HOROSCOPE_CREATION_TIME_KEY = "TODAYS_HOROSCOPE_CREATION_TIME_KEY";
     private static final String TODAYS_HOROSCOPE_TEXT_KEY = "TODAYS_HOROSCOPE_TEXT_KEY";
 
@@ -22,7 +24,16 @@ public final class SharedPreferencesUtils {
         @Nullable String horoscopeText = horoscopePrefs.getString(TODAYS_HOROSCOPE_TEXT_KEY, null);
         if (creationTime > -1 && horoscopeText != null) {
             Horoscope horoscope = new Horoscope(creationTime, horoscopeText);
-            if (TimeUtils.happenedYesterday(creationTime)) {
+            long horoscopeSaveTime = horoscopePrefs.getLong(TODAYS_HOROSCOPE_SAVE_TIME_KEY, -1);
+            if(horoscopeSaveTime == -1) {
+                if(DebugUtils.DEBUG_MODE) {
+                    throw new IllegalStateException("no save time for horoscope");
+                }else {
+                    return horoscope;
+                }
+            }
+
+            if (TimeUtils.happenedYesterday(horoscopeSaveTime)) {
                 return horoscope;
             } else {
                 horoscopePrefs.edit()
@@ -38,6 +49,7 @@ public final class SharedPreferencesUtils {
     @SuppressLint("ApplySharedPref")
     public static void setTodaysHoroscope(Context context, Horoscope horoscope) {
         getHoroscopePrefs(context).edit()
+                .putLong(TODAYS_HOROSCOPE_SAVE_TIME_KEY, System.currentTimeMillis())
                 .putLong(TODAYS_HOROSCOPE_CREATION_TIME_KEY, horoscope.getCreationTimeMillis())
                 .putString(TODAYS_HOROSCOPE_TEXT_KEY, horoscope.getText())
                 .commit();
