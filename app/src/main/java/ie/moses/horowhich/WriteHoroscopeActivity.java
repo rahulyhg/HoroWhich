0269847cb95f7dfe46e6faa008c282fead024b12
abuicke <abuicke@treemetrics.com>
2018-04-12 18:13:54 +0100

    added text counter to horoscope writer activity and buttons now float above keyboard (also changed character limit to 250)
    added text counter to horoscope writer activity and buttons now float
    above keyboard (also changed character limit to 250)

--- app/src/main/java/ie/moses/horowhich/WriteHoroscopeActivity.java
+++ app/src/main/java/ie/moses/horowhich/WriteHoroscopeActivity.java
@@ -1,18 +1,23 @@
 package ie.moses.horowhich;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
 import android.widget.TextView;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-public class WriteHoroscopeActivity extends AppCompatActivity {
+public class WriteHoroscopeActivity extends AppCompatActivity implements TextWatcher {
 
     public static final String FACEBOOK_FRIEND_ID = "FACEBOOK_FRIEND_ID";
 
-    @BindView(R.id.write_horoscope) TextView _writeHoroscope;
+    @BindView(R.id.write_horoscope) EditText _writeHoroscope;
+    @BindView(R.id.character_counter) TextView _characterCounter;
 
     private String _facebookFriendId;
 
@@ -22,6 +27,7 @@ public class WriteHoroscopeActivity extends AppCompatActivity {
         setContentView(R.layout.write_horoscope_activity);
         ButterKnife.bind(this);
         _facebookFriendId = getIntent().getStringExtra(FACEBOOK_FRIEND_ID);
+        _writeHoroscope.addTextChangedListener(this);
     }
 
     @OnClick(R.id.cancel_button)
@@ -33,6 +39,28 @@ public class WriteHoroscopeActivity extends AppCompatActivity {
     public void done() {
         FirebaseUtils.sendHoroscope(_facebookFriendId, _writeHoroscope.getText().toString());
         finish();
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        /**
+         * TODO: If the text was less long before the change no need to set the colour on the counter. (Would be more efficient).
+         * */
+    }
+
+    @Override
+    public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        _characterCounter.setText(String.valueOf(charSequence.length()));
+        if(charSequence.length() == 250) {
+            _characterCounter.setTextColor(Color.rgb(223,7,7));
+        }else {
+            _characterCounter.setTextColor(Color.rgb(134,129,129));
+        }
+    }
+
+    @Override
+    public void afterTextChanged(Editable editable) {
+
     }
 
 }
