88cc471b2d602dcb837b1103dda964df37333511
abuicke <abuicke@treemetrics.com>
2018-04-14 22:15:37 +0100

    small change
    small change

--- app/src/main/java/ie/moses/horowhich/FriendsListAdapter.java
+++ app/src/main/java/ie/moses/horowhich/FriendsListAdapter.java
@@ -1,6 +1,7 @@
 package ie.moses.horowhich;
 
 import android.content.Context;
+import android.support.annotation.IntRange;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.RecyclerView;
